0c5ea58399645a869d3e6467060d13c131e9268f
JesusPastorini <juniorpastorini_@hotmail.com>
2023-03-13 16:58:26 -0300

    Requisito 5 sem refatorar

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  if (animal === undefined) {
+    const newObj = {};
+    data.species.forEach((fe) => {
+      newObj[fe.name] = data.species.find((spe) =>
+        spe.name === fe.name).residents.length;
+    });
+    return newObj;
+  }
+  if (animal.sex === undefined) {
+    return data.species.find((spe) =>
+      spe.name === animal.species).residents.length;
+  }
+  return data.species.find((spe) =>
+    spe.name === animal.species).residents.filter((fil) =>
+    animal.sex === fil.sex).length;
 };
-
 module.exports = countAnimals;
