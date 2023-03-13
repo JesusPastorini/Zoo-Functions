c0178788d6b00950ac3fd65c7a828ff6bb226137
JesusPastorini <juniorpastorini_@hotmail.com>
2023-03-13 18:20:53 -0300

    Requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,19 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Verifica se `handlerElephants` é uma função', () => {
+    expect(typeof handlerElephants).toBe('function');
+  });
+  it('Verifica se o resultado é undefined caso não seja passado nenhum parâmetro.', () => {
+    expect(handlerElephants()).toBe(undefined);
+  });
+  it('Verifica o resultado caso não seja uma string.', () => {
+    expect(handlerElephants(23)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+  it('Verifica o resultado dependendo do parâmetro pasado.', () => {
+    expect(handlerElephants('count')).toBe(4);
+    expect(handlerElephants('names')).toEqual(['Ilana', 'Orval', 'Bea', 'Jefferson']);
+    expect(handlerElephants('averageAge')).toBe(10.5);
+    expect(handlerElephants('location')).toBe('NW');
+  });
+});
