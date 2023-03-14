2bfd8a7e419196ec4fd2ddb316e8d79fbff69229
JesusPastorini <juniorpastorini_@hotmail.com>
2023-03-14 02:43:25 -0300

    Requisito 8 lint

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,41 +1,26 @@
+const { prices } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  const objCont = {};
-  let cont = 0;
-  let cont1 = 0;
-  let cont2 = 0;
-  entrants.forEach(element => {
-      if (element.age < 18 && element.age > 0) {
-        objCont.child = (cont += 1); 
-  } else if (element.age >= 18 && element.age < 50) {
-      objCont.adult = (cont1 += 1);
-  } else if (element.age >= 50) {
-    objCont.senior = (cont2 += 1);}
-});
-return objCont;
+  let child = 0;
+  let adult = 0;
+  let senior = 0;
+  entrants.forEach((element) => {
+    if (element.age < 18 && element.age > 0) {
+      child += 1;
+    } else if (element.age >= 18 && element.age < 50) {
+      adult += 1;
+    } else {
+      senior += 1;
+    }
+  });
+  return { child, adult, senior };
 };
 
 const calculateEntry = (entrants) => {
-  let valor = 0;
-  if (entrants === undefined) return valor;
-  entrants.forEach(element => {
-    if (element.age < 18 && element.age > 0) {
-      valor += data.prices.child;
-} else if (element.age >= 18 && element.age < 50) {
-  valor += data.prices.adult;
-} else if (element.age >= 50) {
-  valor += data.prices.senior;
-}  
-});
-  return parseFloat(valor.toFixed(2));
+  if (entrants === undefined) return 0;
+  const obj = countEntrants(entrants);
+  return obj.child * prices.child + obj.adult * prices.adult + obj.senior * prices.senior;
 };
-console.log(calculateEntry([
-  { name: 'aaaa', age: 5 },
-  { name: 'bbbb', age: 5 },
-  { name: 'cccc', age: 5 },
-  { name: 'dddd', age: 18 },
-  { name: 'eeee', age: 18 },
-  { name: 'ffff', age: 50 },
-]))
+
 module.exports = { calculateEntry, countEntrants };
